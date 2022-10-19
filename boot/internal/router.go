d7cb49997bba2b2c14bba85e182a1c65d031fff2
zhuchangwen <1916147493@qq.com>
2022-10-19 22:33:29 +0800

    补充中间件

--- boot/internal/router.go
+++ boot/internal/router.go
@@ -2,12 +2,19 @@ package internal
 
 import (
 	"github.com/gin-gonic/gin"
+	"zcw-admin-server/middleware"
 	"zcw-admin-server/router"
 )
 
 func Route() *gin.Engine {
 	// 开启gin实例
 	r := gin.New()
+	// 中间件
+	r.Use(
+		middleware.Logger, //日志处理
+		middleware.Cors,   //跨域处理
+		middleware.Error,  //异常处理
+	)
 	// 转载路由
 	router.Api(r)
 
